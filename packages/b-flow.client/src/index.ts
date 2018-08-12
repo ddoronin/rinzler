109c9bdc78fd93a6176274795f8cf7b88e654b8b
Dima Doronin <doronindm@gmail.com>
2018-08-11 21:24:43 -0400

    ws binary type is arraybuffer

--- packages/b-flow.client/src/index.ts
+++ packages/b-flow.client/src/index.ts
@@ -3,18 +3,17 @@ import { Buffer } from 'buffer';
 
 const bson = new BSON();
 const ws = new WebSocket('ws://localhost:8080/');
+ws.binaryType = 'arraybuffer';
 console.info('Connecting...');
 ws.onopen = function () {
     console.info('Connected!');
     ws.send('Hello, dude! I am javascript');
     ws.onmessage = ({data}) => {
         console.info('received:', data);
-        const before = Date.now();
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            console.info(bson.deserialize(Buffer.from(this.result)));
+        if(typeof data === 'object'){
+            const before = Date.now();
+            console.info(bson.deserialize(Buffer.from(data)));
             console.log(`${Date.now() - before}ms`);
         }
-        reader.readAsArrayBuffer(data);
     };
 }
