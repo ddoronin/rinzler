e928b68fdf7bc786a9ac5fc7d30ff92f715f0d69
Dima Doronin <doronindm@gmail.com>
2018-08-13 02:45:41 -0400

    first working prototype

--- packages/b-flow.client/src/index.ts
+++ packages/b-flow.client/src/index.ts
@@ -1,19 +1,44 @@
 import { BSON } from 'bson';
 import { Buffer } from 'buffer';
 
+const fromBSON = (data: Buffer): Object => {
+    const before = Date.now();
+    try{
+        return bson.deserialize(data);
+    } finally{
+        console.log(`${Date.now() - before}ms`);
+    }
+}
+
+const PAYLOAD_SIZE = 1; // byte
+
 const bson = new BSON();
 const ws = new WebSocket('ws://localhost:8080/');
 ws.binaryType = 'arraybuffer';
 console.info('Connecting...');
 ws.onopen = function () {
     console.info('Connected!');
-    ws.send('Hello, dude! I am javascript');
+    const buffer = bson.serialize({
+        collection:'startup_log',
+        options: {
+            fields: {_id: 0, startTime: 1}
+        }
+    });
+    console.log('Buffer.isBuffer', Buffer.isBuffer(buffer));
+    const bodySize = buffer.length;
+    console.log('bodySize', bodySize)
+    const msg = new Buffer(PAYLOAD_SIZE + bodySize);
+    msg.writeUInt8(bodySize, 0);
+    msg.fill(buffer, PAYLOAD_SIZE)
+    ws.send(msg);
     ws.onmessage = ({data}) => {
-        console.info('received:', data);
-        if(typeof data === 'object'){
-            const before = Date.now();
-            console.info(bson.deserialize(Buffer.from(data)));
-            console.log(`${Date.now() - before}ms`);
+        if(data instanceof ArrayBuffer){
+            const buffer = Buffer.from(data);
+            console.log(`STATUS CODE:       ${buffer.readUInt8(0)}`);
+            console.log(`DOCUMENT INDEX:    ${buffer.readUInt16BE(1)}`);
+            const size = buffer.readUInt32BE(3);
+            console.log(`DOCUMENT SIZE:     ${size}`);
+            console.info('DOCUMENT', size > 0 ? fromBSON(buffer.slice(7)): 0);
         }
     };
 }
--- packages/b-flow.mongo/src/server.js
+++ packages/b-flow.mongo/src/server.js
@@ -8,6 +8,83 @@ const config = {
     db: 'local',
     collection: 'startup_log'
 }
+
+const HEADER_SIZE = 7; //bytes
+const STATUS = {
+    OK: 0,
+    ERROR: 1
+}
+/**
+ * PROTOCOL
+ * >-010101010101001010101010->
+ * 
+ * Request:
+ * 
+ * HEADER                   >> 0
+ * +--------------------------+
+ * | PAYLOAD_SIZE       UInt8 |
+ * +--------------------------+
+ * 
+ * PAYLOAD                  >> 1
+ * +--------------------------+
+ * | BSON size = PAYLOAD_SIZE |
+ * +--------------------------+
+ * 
+ * MARKER                   >> 1 + PAYLOAD_SIZE
+ * +--------------------------+
+ * | RequestID - UUID4        |
+ * +--------------------------+
+ * MARKER_SIZE = REQUEST_SIZE - (1 + PAYLOAD_SIZE)
+ * 
+ * Response:
+ * 
+ * HEADER                   >> 0
+ * +--------------------------+
+ * | STATUS_CODE        UInt8 |
+ * | DOCUMENT_INDEX     UInt16|
+ * | DOCUMENT_SIZE      UInt32|
+ * +--------------------------+
+ * 
+ * DOCUMENT                 >> 6
+ * +--------------------------+
+ * | BSON size = DOCUMENT_SIZE|
+ * +--------------------------+
+ * 
+ * MARKER                   >> 6 + DOCUMENT_SIZE
+ * +--------------------------+
+ * | RequestID - UUID4        |
+ * +--------------------------+
+ * 
+ * METADATA                 >> 6 + DOCUMENT_SIZE + MARKER_SIZE
+ * +--------------------------+
+ * | <Buffer ...              |
+ * +--------------------------+
+ */
+
+ /**
+  * 
+  */
+const respond = (code, doci, doc) => {
+    const docSize = doc ? doc.length: 0;
+    console.log('docSize', HEADER_SIZE + docSize);
+    const resp = Buffer.alloc(HEADER_SIZE + docSize);
+
+    // status code
+    resp.writeUInt8(code, 0);
+
+    // document index
+    resp.writeUInt16BE(doci, 1);
+
+    // document size
+    resp.writeUInt32BE(docSize, 3);
+
+    if(docSize > 0) {
+        resp.fill(doc, HEADER_SIZE, docSize);
+    }
+
+    return resp;
+}
+
 const client = new MongoClient(config.url, {raw: true, useNewUrlParser: true});
 client.connect((err, client) => {
     if(err){
@@ -19,22 +96,33 @@ client.connect((err, client) => {
     console.log('Listening on', 8080);
     wss.on('connection', function (ws) {
         ws.send('connected....');
-        ws.on('message', function (msg) {
-            const now = Date.now();
-            console.info('<', msg);
-
+        ws.on('message', function (msg0) {
+            let now = Date.now();
+            const size = msg0.readUInt8(0);
+            const bson = new BSON();
+            const req = bson.deserialize(msg0.slice(1, 1 + size));
+            console.log(req);
             const db = client.db(config.db);
-            const collection = db.collection(config.collection);
-            const readable = collection.find({});
-            readable.on('readable', function() {
-                const cursor = this;
-                let chunk;
-                while (null !== (chunk = cursor.read())) {
-                    //console.log(chunk);
-                    //console.log(new BSON().deserialize(chunk));
-                    //ws.send(`Sending ${chunk.length} bytes of data.`);
-                    console.log(`>> ${Date.now() - now}ms`);
-                    ws.send(chunk, { binary: true });
+            const collection = db.collection(req.collection || config.collection);
+            const cursor = collection.find(req.query || {}, req.options);
+            let count = 0;
+            cursor.on('readable', () => {
+            });
+            cursor.on('end', () => {
+                console.log('!! count is ', count);
+                const done0 = respond(STATUS.OK, count);
+                ws.send(done0, { binary: true });
+            });
+            cursor.forEach(doc => {
+                console.log(`>> ${Date.now() - now}ms`);
+                now = Date.now();
+                const res0 = respond(STATUS.OK, count++, doc);
+                ws.send(res0, { binary: true });
+            }, error => {
+                console.error('error: ', error);
+                if(error !== null){
+                    const error0 = respond(STATUS.ERROR, 0, bson.serialize(error));
+                    ws.send(error0, { binary: true });
                 }
             });
         });
