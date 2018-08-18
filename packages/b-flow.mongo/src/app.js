951d1164e1bde208c2ecad2f8f3ae52ebf688556
Dima Doronin <doronindm@gmail.com>
2018-08-17 20:12:23 -0400

    move method from prototype into the class definition

--- packages/b-flow.mongo/src/app.js
+++ packages/b-flow.mongo/src/app.js
@@ -12,27 +12,27 @@ class App {
         this.mongoClient = mongoClient;
         this.wss = wss;
     }
-}
 
-App.prototype.start = function() {
-    console.info(chalk.yellow('Starting App'));
-    this.wss.on('connection', function(ws) {
-        console.log(chalk.cyanBright('(+1) connection'));
-        ws.on('message', function (msg0) {
-            const msg0_bst = reqProtoReader.getByteShiftTable(msg0);
-            console.log(msg0, msg0_bst);
-
-            const reqCodec = new Codec(reqProtoReader, msg0);
-            const requestId = reqCodec.readAsString('MARKER');
-            const payload = reqCodec.readAsJSON('BODY');
-
-            console.log('requestId', chalk.green(requestId));
-            console.log('payload  ', chalk.green(
-                JSON.stringify(payload, null, '\t')
-            ));
-            // TODO: connect to mongo.
+    start() {
+        console.info(chalk.yellow('Starting App'));
+        this.wss.on('connection', function(ws) {
+            console.log(chalk.cyanBright('(+1) connection'));
+            ws.on('message', function (msg0) {
+                const msg0_bst = reqProtoReader.getByteShiftTable(msg0);
+                console.log(msg0, msg0_bst);
+    
+                const reqCodec = new Codec(reqProtoReader, msg0);
+                const requestId = reqCodec.readAsString('MARKER');
+                const payload = reqCodec.readAsJSON('BODY');
+    
+                console.log('requestId', chalk.green(requestId));
+                console.log('payload  ', chalk.green(
+                    JSON.stringify(payload, null, '\t')
+                ));
+                // TODO: connect to mongo.
+            });
         });
-    });
+    }
 }
 
 exports.App = App;
