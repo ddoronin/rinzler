c0e9d775787999226c624c69dd5280c7fac9a807
Dima Doronin <doronindm@gmail.com>
2018-09-01 02:19:00 -0400

    add more aliases

--- packages/t-buf/src/types.ts
+++ packages/t-buf/src/types.ts
@@ -29,18 +29,31 @@ export const string = _('String');
 
 // alias
 export const u8     = uint8;
+export const ubyte  = uint8;
+
 export const u16    = uint16;
+export const ushort = uint16;
+
 export const u32    = uint32;
+export const uint   = uint32;
 
 export const i8     = int8;
+export const byte   = int8;
+
 export const i16    = int16;
+export const short  = int16;
+
 export const i32    = int32;
+export const int    = int32;
 
 export const f      = float;
 export const d      = double;
+export const number = double;
 
 export const bool   = boolean;
 
 export const s      = string;
+export const str    = string;
 
 export const o      = bson;
+export const object = bson;
